02f7e395c44e408717007d43a6b68c73b4e51684
Anuj0725 <anujmadhani1@gmail.com>
2026-07-08 11:32:52 +0530

    log added for exception handling

--- src/main/java/com/Project/url_shortener/config/GlobalExceptionHandler.java
+++ src/main/java/com/Project/url_shortener/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.Project.url_shortener.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> handleBadRequest(IllegalArgumentException e){
@@ -36,6 +38,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleEveryOtherException(Exception e){
+        log.error("Unhandled exception", e);
         return new ResponseEntity<>("Something went wrong",HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
